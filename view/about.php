305b52893bf09a400ab32702b92484030601963e
marcosilvadf <106616336+marcosilvadf@users.noreply.github.com>
2022-07-11 19:52:12 -0300

    add about

--- view/about.php
+++ view/about.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/mobile.css">
     <link rel="stylesheet" href="../lib/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../css/about.css">
     <script src="../js/menu.js" defer></script>
     <script src="../js/menuFloat.js" defer></script>
     <script src="../js/loadProfileSQLView.js" defer></script>
@@ -49,7 +50,54 @@
         </div>
     </div>
 
-    <main></main>
+    <main>
+        <h1>História</h1>
+        <p>Sempre quis ter um site e nunca tinha um objetivo em específico, então veio a ideia de criar um site onde se pareça com uma rede social para a comunidade que adora GTA, um dos meus canais, em específico este <a href="https://www.youtube.com/channel/UCeuhEjOoQbWocjae2FJ7WYQ" target="_blank">aqui</a> é o responsável por eu ter tido a ideia, se não ficou claro o criador do site é o próprio maiorzin!</p>
+
+        <h1>Equipe</h1>
+        <p>Gostaria de falar que há uma grande equipe que me ajuda nos vídeos, posts, instagram, youtube, site, mas sou só eu mesmo!</p>
+
+        <h1>Ambiente</h1>
+        <p>Gosto de chamar cada uma das minhas redes sociais e canais de uma parte da minha vida, mas a minha vida não se resumo a só GTA, aqui é onde vocês podem ver tudo que faz parte da minha vida. Ah, mais coisa, basta clicar em um item abaixo para ser direcionado a ele.</p>
+
+        <h3 style="background-image: linear-gradient(90deg, #FF8C00, #FF0080);">Instagram</h3>
+
+        <ul>
+            <li><a href="">Fã Clube (Esse é o perfil do clube do maiorzin)</a></li>
+            <li><a href="">Empresas Da Internet (Esse é o perfil com dicas de canais, perfils, programação, empresas e etc)</a></li>
+            <li><a href="">Perfil Pessoal</a></li>
+        </ul>
+
+        <h3 style="background-color: red;">Youtube</h3>
+
+        <ul>
+            <li><a href="">Canal Oasis (Canal de entretenimento, comédia e vlogs)</a></li>
+            <li><a href="">Canal Marcos Silva (Canal de tecnologia e Unboxings)</a></li>
+            <li><a href="">Canal de Gameplay</a></li>
+            <li><a href="">Canal Maiorzin</a></li>
+        </ul>
+
+        <h3 style="background-color: #2193b0;">Outras redes sociais</h3>
+
+        <ul>
+            <li><a href="">LinkedIn</a></li>
+            <li><a href="">GitHub</a></li>
+            <li><a href="">Twitter</a></li>
+            <li><a href="">Facebook</a></li>
+            <li><a href=""></a></li>
+        </ul>
+
+        <h3>Termos de uso</h3>
+
+        Esse é meu primeiro site, então, peguem leve :)
+
+        <ul>
+            <li>Esse site só funciona com cookies então ao criar uma conta vocês devem aceitar os cookies.</li>
+            <li>O site ainda está em desenvolvimento então poderão haver mudanças.</li>
+            <li>Problemas com direitos serão de responsabilidade do usuário que fez a postagem.</li>
+            <li>O usuário correrá o risco de perder a conta se: postar fotos com público para maiores de 18; denunciar muitas vezes sem comprovação, ser denunciado e ser comprovado que há muitas postagens de outras pessoas, fizer mal uso das postagens, se for decidido pelo administrador.</li>
+        </ul>
+    </main>
 
     <footer>
         <div id="nav" style="display: none;"></div>
